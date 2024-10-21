1c0b183e6e1e76c5213013ad4e72844af83d5277
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-10-21 14:22:24 -0300

    add daemon thread example

--- src/main/java/me/calebeoliveira/StartingThreads.java
+++ src/main/java/me/calebeoliveira/StartingThreads.java
@@ -1,21 +1,31 @@
 package me.calebeoliveira;
 
-class Runnable1 implements Runnable {
-    public void execute() {
+class Runnable1 extends Thread {
+    public void execute() throws InterruptedException {
         for (int i = 0; i < 10; i++) {
+            Thread.sleep(1000);
             System.out.println("Runner1: " + i);
         }
     }
 
     @Override
     public void run() {
-        execute();
+        try {
+            execute();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
-class Runnable2 implements Runnable {
+class Runnable2 extends Thread {
     public void execute() {
         for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
             System.out.println("Runner2: " + i);
         }
     }
@@ -28,10 +38,19 @@ class Runnable2 implements Runnable {
 
 public class StartingThreads {
     public static void main(String[] args) {
-        Thread t1 = new Thread(new Runnable1());
-        Thread t2 = new Thread(new Runnable2());
+        Thread t1 = new Runnable1();
+        Thread t2 = new Runnable2();
 
         t1.start();
         t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Finished with the Threads...");
     }
 }
---
+++ src/main/java/me/calebeoliveira/daemon/WorkerThreadEx.java
@@ -0,0 +1,42 @@
+package me.calebeoliveira.daemon;
+
+class NormalWorker implements Runnable {
+
+    @Override
+    public void run() {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Normal thread finishes execution");
+    }
+}
+
+class DaemonWorker implements Runnable {
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Daemon thread is running");
+        }
+    }
+}
+
+public class WorkerThreadEx {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new DaemonWorker());
+        Thread t2 = new Thread(new NormalWorker());
+        t1.setDaemon(true);
+
+        t1.start();
+        t2.start();
+
+        System.out.println(t1.isDaemon());
+    }
+}

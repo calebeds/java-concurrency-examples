ac71c941a163f58ab820084e4d9b1911631a69c7
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-10-23 16:56:59 -0300

    add stopping executors example

---
+++ src/main/java/me/calebeoliveira/threadpool/StopingExecutors.java
@@ -0,0 +1,48 @@
+package me.calebeoliveira.threadpool;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class Work implements Runnable {
+
+    private int id;
+
+    public Work(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Taks with id " + id + " is being worked - thread id: " + Thread.currentThread().getId());
+        long duration = (long) (Math.random() * 5);
+
+        try {
+            TimeUnit.SECONDS.sleep(duration);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
+
+public class StopingExecutors {
+    public static void main(String[] args) {
+
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
+        for (int i = 0; i < 100; i++) {
+            executor.execute(new Work(i + 1));
+        }
+
+        executor.shutdown();
+
+        try {
+            if(!executor.awaitTermination(1000, TimeUnit.MILLISECONDS)) {
+                //executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+        }
+    }
+}

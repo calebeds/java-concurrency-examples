3144a24cfbe1f3892ae406150aac6dc3dc3a04b1
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-10-23 14:52:51 -0300

    add fixed thread pool example

---
+++ src/main/java/me/calebeoliveira/fixedthreadpool/singlethreadexecutor/FixedThreadPool.java
@@ -0,0 +1,36 @@
+package me.calebeoliveira.fixedthreadpool.singlethreadexecutor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class Task implements Runnable {
+    private int id;
+
+    public Task(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Taks with id " + id + " is being worked - thread id: " + Thread.currentThread().getId());
+        long duration = (long) (Math.random() * 5);
+        try {
+            TimeUnit.SECONDS.sleep(duration);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+public class FixedThreadPool {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+
+        for (int i = 0; i < 10; i++) {
+            executor.execute(new Task(i + 1));
+        }
+
+        executor.shutdown();
+    }
+}

d8b921f584db753dc690319c0e038cdc6826a752
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-10-28 13:24:52 -0300

    add merge sort implementantion

---
+++ src/main/java/me/calebeoliveira/mergesort/MergeSortExample.java
@@ -0,0 +1,11 @@
+package me.calebeoliveira.mergesort;
+
+public class MergeSortExample {
+    public static void main(String[] args) {
+        int[] nums = {5, -1, 0, 7, 2, 3, 2, 1, 0, 1, 2};
+
+        MergeSort sort = new MergeSort(nums);
+        sort.sort();
+        sort.showArray();
+    }
+}
